89c297bfdc107a9b9e218b441c6ebf4e8087f133
Nguyễn Đăng Hùng <42560696+dhungk39@users.noreply.github.com>
2022-03-24 22:15:51 +0700

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -19,7 +19,7 @@ data "aws_ami" "ubuntu" {
 }
 
 resource "aws_instance" "ubuntu"{
-    ami = data.aws.ubuntu.id
+    ami = data.aws_ami.ubuntu.id
     instance_type = var.instance_type
 
     tags = {
