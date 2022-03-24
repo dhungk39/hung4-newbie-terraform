2558d699f430dc738ab7ea0770c3605e705a3d8f
Nguyễn Đăng Hùng <42560696+dhungk39@users.noreply.github.com>
2022-03-24 22:25:52 +0700

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -18,9 +18,9 @@ data "aws_ami" "ubuntu" {
     owners  = ["099720109477"]
 }
 
-resource "aws_instance" "ubuntu"{
-    ami = data.aws_ami.ubuntu.id
-    instance_type = var.instance_type
+resource "aws_instance" "ubuntu" {
+    ami             = data.aws_ami.ubuntu.id
+    instance_type   = var.instance_type
 
     tags = {
         Name = var.instance_name
